a93c3b504e3faf248c4f9aa6ba9587236ae0f2b7
unknown <xuheyao@jiuqi.com.cn>
2017-01-17 11:03:40 +0800

    登录页面 添加回车登录 登录点击事件 移植函数到js文件中

---
+++ app/js/signin.js
@@ -0,0 +1,40 @@
+/**
+ * 表单验证函数
+ */
+function validateForm() {
+    var urlValue = document.forms["login"]["url"].value;
+    var patt = /^(localhost|(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})):\d{4,5}$/g;
+    var adminValue = document.forms["login"]["username"].value;
+    var passwordValue = document.forms["login"]["password"].value;
+    if (urlValue == null || urlValue == "") {
+        window.alert("请填写URL！", "提示");
+        return false;
+    } else if (adminValue == null || adminValue == "") {
+        window.alert("请填写账户名！", "提示");
+        return false;
+    } else if (passwordValue == null || passwordValue == "") {
+        window.alert("请填写密码！", "提示");
+        return false;
+    } else if (urlValue.trim().match(patt) == null) {
+        window.alert("不是一个有效的 url 地址！", "提示");
+        return false;
+    }
+    return true;
+}
+
+/**
+ * 登录按钮点击事件
+ */
+function signIn() {
+    if (validateForm()) {
+        alert("hehe");
+    }
+}
+
+/**
+ * 回车自动登录
+ */
+function keySignIn() {
+    if (event.keyCode == 13)
+        signIn();
+}
