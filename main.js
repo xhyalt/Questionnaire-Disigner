9a71158b1e3a2375e4d92422370dcae59abb70b6
unknown <xuheyao@jiuqi.com.cn>
2017-01-19 14:46:14 +0800

    规范注释 屏幕大小动态决定窗口大小

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
 const electron = require('electron');
-//控制应用生命周期的模块
+/*控制应用生命周期的模块*/
 const app = electron.app;
-//创建原生浏览器窗口的模块
+/*创建原生浏览器窗口的模块*/
 const BrowserWindow = electron.BrowserWindow;
-//保持其全局引用，不然JS被GC，windows自动关闭
+/*保持其全局引用，不然JS被GC，windows自动关闭*/
 var mainWindow = null;
 
-//当所有窗口关闭了，退出
+/*当所有窗口关闭了，退出*/
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
         app.quit();
     }
 });
 
-//当Electron初始化完毕并准备创建浏览器窗口时
-//这个方法就被调用
+/**
+ * 当Electron初始化完毕并准备创建浏览器窗口时
+ * 这个方法就被调用
+ */
 app.on('ready', function() {
-    //创建浏览器窗口
+    /*获取屏幕大小*/
+    var electronScreen = electron.screen;
+    var size = electronScreen.getPrimaryDisplay().workAreaSize;
+    /*创建客户端窗口*/
     mainWindow = new BrowserWindow({
         title: "问卷设计器",
-        width: 12000,
-        height: 8000,
+        width: size.width,
+        height: size.height,
         minWidth: 1200,
         minHeight: 800,
         // autoHideMenuBar: true
     });
 
-    //加载index.html
+    /*加载login.html*/
     mainWindow.loadURL('file://' + __dirname + './app/login.html');
 
-    //当关闭该window时，这个事件被发出
+    /*当关闭该window时，这个事件被发出*/
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
