48d2f5922345963f20e3adea58fc810d0cd401d7
Emily <xuheyao_emily@163.com>
2017-01-22 15:36:01 +0800

    主进程 添加最大化 通信模块

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ const electron = require('electron');
 const app = electron.app;
 /*创建原生浏览器窗口的模块*/
 const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const glob = require('glob');
+const ipc = require('electron').ipcMain;
 /*保持其全局引用，不然JS被GC，windows自动关闭*/
 var mainWindow = null;
+
+// const quesSqlite = require('./app/js/quesSqlite.js');
+
+ipc.on('get-app-path', function(event) {
+    event.sender.send('got-app-path', app.getAppPath())
+})
 
 /*当所有窗口关闭了，退出*/
 app.on('window-all-closed', function() {
@@ -18,9 +27,11 @@ app.on('window-all-closed', function() {
  * 这个方法就被调用
  */
 app.on('ready', function() {
+
     /*获取屏幕大小*/
     var electronScreen = electron.screen;
     var size = electronScreen.getPrimaryDisplay().workAreaSize;
+
     /*创建客户端窗口*/
     mainWindow = new BrowserWindow({
         title: "问卷设计器",
@@ -28,14 +39,22 @@ app.on('ready', function() {
         height: size.height,
         minWidth: 1200,
         minHeight: 800,
-        // autoHideMenuBar: true
+        show: false
     });
 
-    /*加载login.html*/
-    mainWindow.loadURL('file://' + __dirname + './app/login.html');
+    /*加载进入页*/
+    mainWindow.loadURL(`file://${__dirname}/app/login.html`);
 
     /*当关闭该window时，这个事件被发出*/
     mainWindow.on('closed', function() {
         mainWindow = null;
+    });
+
+    /*在所有都加载完成后，再显示窗口并聚焦提醒用户*/
+    mainWindow.on('ready-to-show', function() {
+        mainWindow.show();
+        mainWindow.focus();
+        /*打开窗口后再最大化*/
+        mainWindow.maximize();
     });
 });
