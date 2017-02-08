cf5b2c0520b878e389aafdac618986f2a0ea911f
Emily <xuheyao_emily@163.com>
2017-02-08 16:09:17 +0800

    完成登录的判断逻辑和涉及的数据库函数

--- app/js/quesSqlite.js
+++ app/js/quesSqlite.js
@@ -10,37 +10,107 @@ const dbFile = "./quesSqlite.db";
 const db = new sqlite3.Database(dbFile);
 
 /**
- * 登录时初始化数据库
+ * 初始化数据库函数
  * @public
  * @param  GlobalData
- * @param  cb
+ * @param  cb callback
  * @return
  */
 function initDB(GlobalData, cb) {
 
+    console.log("正在初始化数据库 initDB")
     checkTable(function(res1) {
-        alert("res1" + res1.data);
-        // console.log("res1" + res1.data);
+        console.log("检查表是否存在" + res1.success);
         if (res1.success == false) {
+
             /*不存在数据库，调用建表函数*/
+            console.log("表不存在，建表");
             createTable(function(res2) {
-                alert("res2" + res2.success);
-                /*插入该条数据*/
+                console.log("建表是否成功 " + res2.success);
                 if (res2.success == true) {
+                    /*建表成功*/
+                    console.log("建表成功");
+                    /*插入该条数据*/
                     insertUser(GlobalData, function(res3) {
-                        if (res3.success == false)
-                            alert("res3" + res3.data);
-                        else alert("res3" + res3.success);
+                        if (res3.success == false) {
+                            /*插入数据失败*/
+                            console.log("插入数据失败");
+                            if (cb) {
+                                cb({
+                                    success: false,
+                                    data: "插入数据失败"
+                                });
+                            }
+                        } else {
+                            console.log("插入数据成功");
+                            if (cb) {
+                                cb({
+                                    success: true
+                                });
+                            }
+                        }
                     });
+                } else {
+                    /*建表不成功*/
+                    if (cb) {
+                        cb({
+                            success: false,
+                            data: "建表不成功"
+                        });
+                    }
                 }
             });
         } else {
             /*已存在建表函数，调用查找函数*/
-            /*判断表中是否存在该数据*/
+            console.log("表已存在");
+            /*判断表中是否存在该用户数据*/
             checkUser(GlobalData, function(res2) {
-                alert(res2.success);
+                console.log("该用户是否存在 " + res2.success);
+                if (res2.success == true) {
+                    /*该用户存在，更新用户数据*/
+                    updateUser(GlobalData, function(res3) {
+                        console.log("更新用户数据是否成功" + res3.success);
+                        if (res3.success == true) {
+                            console.log("更新用户数据成功");
+                            if (cb) {
+                                cb({
+                                    success: true
+                                });
+                            }
+                        } else {
+                            console.log("更新用户数据失败");
+                            if (cb) {
+                                cb({
+                                    success: false,
+                                    data: "更新用户数据失败"
+                                });
+                            }
+                        }
+                    });
+
+                } else {
+                    /*该用户不存在，插入用户数据*/
+                    insertUser(GlobalData, function(res3) {
+                        if (res3.success == false) {
+                            /*插入数据失败*/
+                            console.log("插入数据失败");
+                            if (cb) {
+                                cb({
+                                    success: false,
+                                    data: "插入数据失败"
+                                });
+                            }
+                        } else {
+                            console.log("插入数据成功");
+                            if (cb) {
+                                cb({
+                                    success: true
+                                });
+                            }
+                        }
+                    });
+                }
             });
-            alert("已存在表表");
         }
     });
 }
@@ -49,10 +119,11 @@ function initDB(GlobalData, cb) {
  * 检查表是否存在
  * 若不存在建表
  * @private
- * @param  cb
+ * @param  cb callback
  * @return
  */
 function checkTable(cb) {
+    console.log("正在检查是否存在表 checkTable");
     if (db) {
         db.get("select 1 from USER", function(err, row) {
             if (err) {
@@ -71,69 +142,99 @@ function checkTable(cb) {
 
 /**
  * 创建表的函数
+ * @private
+ * @param cb callback
+ * @return
  */
 function createTable(cb) {
-    alert("sqlite3 " + sqlite3);
-    alert("db = " + db);
+    console.log("正在创建表 createTable");
     /*创建用户信息表*/
     db.serialize(function() {
-        db.get("create table USER(URL TEXT, user TEXT, userName TEXT, pwd TEXT, token TEXT)", function(err, row) {
-            alert("hehehehe");
-            if (err) {
-                alert("There is something wrong");
-                cb({
-                    success: false,
-                    data: err.message
-                });
-            } else {
-                alert("You got it");
+        try {
+            db.run("create table USER(URL TEXT, user TEXT, userName TEXT, pwd TEXT, token TEXT)");
+
+            /*表单创建成功*/
+            if (cb) {
                 cb({
                     success: true
                 });
             }
-        });
+        } catch (err) {
+            /*表单创建失败*/
+            if (cb) {
+                cb({
+                    success: false
+                });
+            }
+        }
     });
 }
 
 /**
  * 检查当前用户表中是否存在该用户
+ * @private
+ * @param  GlobalData 用户基础数据
+ * @param  cb 回调函数
+ * @return
  */
 function checkUser(GlobalData, cb) {
-    alert("checkUser1");
-    db.each("select rowid AS id,user,URL from USER where user = ? and URL = ?", [GlobalData.user, GlobalData.urlRoot], function(err, row) {
-        alert(row.id + ": " + row.URL + " " + row.user + " " + row.pwd);
-});
-}
-
-function insertUser(GlobalData, cb) {
-    alert("GlobalData.token = " + GlobalData.token);
-    db.run("insert into USER(URL, user, userName, pwd, token) values(?, ?, ?, ?, ?)", [GlobalData.urlRoot, GlobalData.user, GlobalData.userName, GlobalData.pwd, GlobalData.token], function(err) {
+    console.log("正在检查该用户是否存在 checkUser");
+    db.each("select 1 from USER where user = ? and URL = ?", [GlobalData.user, GlobalData.urlRoot], function(err, row) {
         if (err) {
-            alert(err.message);
             cb({
-                success: false,
-                data: err.message
-            });
-        } else {
-            cb({
-                success: true,
-                data: row
-            });
-        }
-    });
-}
-
-function updateUser(GlobalData) {
-    db.get("update USER set token = ?, userName = ?, pwd = ? where URL = ? and user = ?", [GlobalData.token, GlobalData.userName, GlobalData.pwd, GlobalData.urlRoot, GlobalData.user], function(err, row) {
-        if (err) {
-            return cb({
                 success: false,
                 data: err
             });
         }
-        return cb({
+        cb({
             success: true,
             data: row
+        });
+    });
+}
+
+/**
+ * 在用户表中插入该用户数据
+ * @private
+ * @param  GlobalData 用户基础数据
+ * @param  cb callback
+ * @return
+ */
+function insertUser(GlobalData, cb) {
+    console.log("正在插入该用户数据 insertUser");
+    db.run("insert into USER(URL, user, userName, pwd, token) values(?, ?, ?, ?, ?)", [GlobalData.urlRoot, GlobalData.user, GlobalData.userName, GlobalData.pwd, GlobalData.token], function(err) {
+        if (err) {
+            console.log(err.message);
+            cb({
+                success: false,
+                data: err.message
+            });
+        }
+        cb({
+            success: true,
+        });
+
+    });
+}
+
+/**
+ * 更新该用户数据
+ * @private
+ * @param GlobalData 用户基础数据
+ * @param cb callback
+ * @return
+ */
+function updateUser(GlobalData, cb) {
+    console.log("正在更新该用户数据 updateUser");
+    db.get("update USER set token = ?, userName = ?, pwd = ? where URL = ? and user = ?", [GlobalData.token, GlobalData.userName, GlobalData.pwd, GlobalData.urlRoot, GlobalData.user], function(err) {
+        if (err) {
+            cb({
+                success: false,
+                data: err.message
+            });
+        }
+        cb({
+            success: true
         });
     });
 }
