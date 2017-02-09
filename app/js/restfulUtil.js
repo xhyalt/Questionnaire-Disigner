af355eb32f17ac07ba7c1faf036f760df64d8482
Emily <xuheyao_emily@163.com>
2017-02-09 09:34:50 +0800

    移入获取登录URL的函数

--- app/js/restfulUtil.js
+++ app/js/restfulUtil.js
@@ -5,12 +5,24 @@ var url = require('url');
 const quesSqlite = require("./quesSqlite.js");
 
 /**
+ * 获取Token的URL
+ * @private
+ * @param GlobalData
+ * @return
+ */
+function __getTokenUrl(GlobalData) {
+    var url = `http://` + GlobalData.urlRoot + `/jqrapi/auth/login/${GlobalData.user}?pwd=${GlobalData.pwd}&src=${GlobalData.src}&devid=${GlobalData.devid}`;
+    return url;
+}
+
+/**
  * 登录验证函数
  * 登录成功则choken的JSON对象
  * 登录不成功则根据error信息判断错误
  */
-function getToken(url, cb) {
+function getToken(GlobalData, cb) {
     var http = require('http');
+    var url = __getTokenUrl(GlobalData);
 
     http.get(url, (res) => {
         res.setEncoding('utf8');
