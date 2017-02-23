322b1d1f5f71fd9b9679c2a553f6dfe1a0cda133
Emily <xuheyao_emily@163.com>
2017-02-23 16:11:20 +0800

    设计界面 将替换图片的函数移动到js文件中 规范属性

--- app/js/design.js
+++ app/js/design.js
@@ -7,26 +7,7 @@
 var GlobalData = null;
 
 $(function() {
-    var LPB = window.LPB = window.LPB || {
-        plugins: [],
-        genSource: function() {
-            var $temptxt = $("<div>").html($("#build").html());
-            $($temptxt).find(".subType").attr({
-                "title": null,
-                "data-original-title": null,
-                "data-type": null,
-                "data-content": null,
-                "rel": null,
-                "trigger": null,
-                "style": null
-            });
-            $($temptxt).find(".subType").removeClass("subType");
-            $($temptxt).find("form").attr({
-                "id": null,
-                "style": null
-            });
-        }
-    };
+
     /*获取用户基本信息*/
     __getGlobalData(function(res) {
         if (res.success == true) {
@@ -36,19 +17,8 @@ $(function() {
         }
     });
 
-
-
-    /*鼠标移入题目显示小图标 移出题目隐藏小图标*/
-    $(".subject").mouseover(function() {
-        var td = $(this);
-        // td.$("#up").show();
-    }).mouseout(function() {
-        var td = $(this);
-        // td.$("#up").hide();
-    });
-
-    /*鼠标移入移出详述 变化颜色*/
-    $("#target").on("mouseover", ".textBox", function() {
+    /*鼠标移入移出可编辑条目 变化颜色*/
+    $("#middle").on("mouseover", ".textBox", function() {
         var td = $(this);
         td.css("background-color", "#b4fbef");
     }).on("mouseout", ".textBox", function() {
@@ -56,8 +26,32 @@ $(function() {
         td.css("background-color", "#fff");
     });
 
+    /*题目中的小图 鼠标移入显示移出隐藏*/
+    $("#target").on("mouseover", ".subject", function() {
+        var td = $(this);
+        $(this).find("img").css({
+            "visibility": "visible"
+        });
+    }).on("mouseout", ".subject", function() {
+        var td = $(this);
+        $(this).find("img").css({
+            "visibility": "hidden"
+        });
+    });
+
+    // $(".subject").on("mouseover", "#up", function(){
+    //     console.log("识别出替换的img");
+    //     MM_swapImage('up','','./images/main_01_up_on.png',1)
+    // }).on("mouseout", "#up", function(){
+    //     MM_swapImgRestore();
+    // });
+
+    $("#target").on("click", "#up", function() {
+        console.log("执行up点击事件");
+    });
+
     /*题头详述 点击编辑*/
-    $("#headDetailID").click(function() {
+    $("#titleBox").on("click", "#headDetailID", function() {
         var td = $(this);
         var txt = td.text();
         var input = $(`<input id='detailTextInput' type='text' value='` + txt + `'/>`);
@@ -90,7 +84,7 @@ $(function() {
     });
 
     /*单选题题干 点击编辑*/
-    $(".radioStemText").click(function() {
+    $("#target").on("click", ".radioStemText", function() {
         var td = $(this);
         var txt = td.text();
         var input = $(`<input class='radioStemTextInput' type='text' value='` + txt + `'/>`);
@@ -119,8 +113,9 @@ $(function() {
         });
     });
 
+
     /*单选题选项 点击编辑*/
-    $(".radioItemText").click(function() {
+    $("#target").on("click", ".radioItemText", function() {
         var td = $(this);
         var txt = td.text();
         var input = $(`<input class='radioItemTextInput' type='text' value='` + txt + `'/>`);
@@ -146,162 +141,6 @@ $(function() {
             } else {
                 td.html(newtxt);
             }
-        });
-    });
-});
-
-$(document).ready(function() {
-
-    /*题型点击事件*/
-    $(".menuItemBox").delegate(".subType", "mousedown", function(md) {
-        $(".popover").remove();
-        md.preventDefault();
-        var tops = [];
-        var mouseX = md.pageX;
-        var mouseY = md.pageY;
-        var $temp;
-        var timeout;
-        var $this = $(this);
-        var delays = {
-            main: 0,
-            form: 120
-        };
-        var type;
-
-        if ($this.parent().parent().parent().attr("class") === "menuItemBox") {
-            type = "main";
-        } else {
-            type = "form";
-        }
-
-        var delayed = setTimeout(function() {
-            console.log("var delayed = setTimeout");
-            // if (type === "main") {
-            $temp = $(`<div class="cloth"></div>`).append(radioDiv);
-            // } else {
-            //     if ($this.attr("id") !== "legend") {
-            //         $temp = $($this).append(radioDiv);
-            //     }
-            // }
-
-            $("body").append($temp);
-
-            $temp.css({
-                "position": "absolute",
-                "top": mouseY - ($temp.height() / 2) + "px",
-                "left": mouseX - ($temp.width() / 2) + "px",
-                "opacity": "0.9"
-            }).show()
-
-            var half_box_height = ($temp.height() / 2);
-            var half_box_width = ($temp.width() / 2);
-            var $target = $("#target");
-            var tar_pos = $target.position();
-            var $target_subType = $("#target .subject");
-
-            /*题型 鼠标移动触发事件*/
-            $(document).delegate("body", "mousemove", function(mm) {
-
-                var mm_mouseX = mm.pageX;
-                var mm_mouseY = mm.pageY;
-                $temp.css({
-                    "top": mm_mouseY - half_box_height + "px",
-                    "left": mm_mouseX - half_box_width + "px"
-                });
-
-                /*鼠标在target范围之内*/
-                if (mm_mouseX > tar_pos.left &&
-                    mm_mouseX < tar_pos.left + $target.width() &&
-                    mm_mouseY > tar_pos.top &&
-                    mm_mouseY < tar_pos.top + $target.height()) {
-                    $target_subType.css({
-                        "border-top": "none",
-                        "border-bottom": "none"
-                    });
-                    tops = $.grep($target_subType, function(e) {
-                        return ($(e).position().top - mm_mouseY + half_box_height > 0 && $(e).attr("id") !== "target");
-                    });
-                    if (tops.length > 0) {
-                        /*识别位置在上半部分*/
-                        console.log("tops.length = " + tops.length);
-                        $(tops[0]).css("border-top", "5px solid #1ABC9C");
-                    } else {
-                        if ($target_subType.length > 0) {
-                            /*识别位置在下半部分*/
-                            console.log("$target_subType.length = " + $target_subType.length);
-                            $($target_subType[$target_subType.length - 1]).css("border-bottom", "5px solid #1ABC9C");
-                        }
-                        else{
-                          $("#emptyBox").css({
-                              "border-top": "5px solid #1ABC9C",
-                              "border-bottom": "none"
-                          });
-                        }
-                    }
-                } else {
-                    $target_subType.css({
-                        "border-top": "none",
-                        "border-bottom": "none"
-                    });
-                }
-            });
-
-            /*松开鼠标触发事件*/
-            $("body").delegate(".subject", "mouseup", function(mu) {
-                mu.preventDefault();
-
-                var mu_mouseX = mu.pageX;
-                var mu_mouseY = mu.pageY;
-                var tar_pos = $target.position();
-
-                $("#target .subject").css({
-                    "border-top": "none",
-                    "border-bottom": "none"
-                });
-
-                /*鼠标是否进入target范围*/
-                if (mu_mouseX > tar_pos.left &&
-                    mu_mouseX < tar_pos.left + $target.width() &&
-                    mu_mouseY > tar_pos.top &&
-                    mu_mouseY < tar_pos.top + $target.height()
-                ) {
-                    $temp.attr("style", null);
-                    // where to add
-                    console.log("top.length = "+tops.length);
-                    if (tops.length > 0) {
-                        $($temp.html()).insertBefore(tops[0]);
-                    } else {
-                        $("#target").append($temp.html());
-                        $("#emptyBox").remove();
-                    }
-                } else {
-                    $("#target .subject").css({
-                        "border-top": "none",
-                        "border-bottom": "none"
-                    });
-                    tops = [];
-                }
-
-                $(document).undelegate("body", "mousemove");
-                $("body").undelegate(".subject", "mouseup");
-                $("#target .subType").popover({
-                    trigger: "manual"
-                });
-                $temp.remove();
-                LPB.genSource();
-            });
-        }, delays[type]);
-
-
-        $(document).mouseup(function() {
-            console.log("$(document).mouseup");
-            clearInterval(delayed);
-            return false;
-        });
-        $(this).mouseout(function() {
-            console.log("$(this).mouseout");
-            clearInterval(delayed);
-            return false;
         });
     });
 });
@@ -343,8 +182,49 @@ function __getGlobalData(cb) {
     }
 }
 
-function setEmptyBox() {
-    /*如果设计器中一道题也没有 放置空盒*/
+function MM_swapImgRestore() {
+    var i, x, a = document.MM_sr;
+    for (i = 0; a && i < a.length && (x = a[i]) && x.oSrc; i++) x.src = x.oSrc;
+}
+
+function MM_preloadImages() {
+    var d = document;
+    if (d.images) {
+        if (!d.MM_p) d.MM_p = new Array();
+        var i, j = d.MM_p.length,
+            a = MM_preloadImages.arguments;
+        for (i = 0; i < a.length; i++)
+            if (a[i].indexOf("#") != 0) {
+                d.MM_p[j] = new Image;
+                d.MM_p[j++].src = a[i];
+            }
+    }
+}
+
+function MM_findObj(n, d) {
+    var p, i, x;
+    if (!d) d = document;
+    if ((p = n.indexOf("?")) > 0 && parent.frames.length) {
+        d = parent.frames[n.substring(p + 1)].document;
+        n = n.substring(0, p);
+    }
+    if (!(x = d[n]) && d.all) x = d.all[n];
+    for (i = 0; !x && i < d.forms.length; i++) x = d.forms[i][n];
+    for (i = 0; !x && d.layers && i < d.layers.length; i++) x = MM_findObj(n, d.layers[i].document);
+    if (!x && d.getElementById) x = d.getElementById(n);
+    return x;
+}
+
+function MM_swapImage() {
+    var i, j = 0,
+        x, a = MM_swapImage.arguments;
+    document.MM_sr = new Array;
+    for (i = 0; i < (a.length - 2); i += 3)
+        if ((x = MM_findObj(a[i])) != null) {
+            document.MM_sr[j++] = x;
+            if (!x.oSrc) x.oSrc = x.src;
+            x.src = a[i + 2];
+        }
 }
 
 function getQuestionnairesData() {
@@ -352,28 +232,3 @@ function getQuestionnairesData() {
         console.log(JSON.stringify(res));
     });
 }
-
-const radioDiv = `
-<div class="radioDiv subject">
-    <div class="leftSetup">
-        <h4>Q</h4>
-        <img id="up" src="./images/main_01_up_off.png" alt="" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('up','','./images/main_01_up_on.png',1)">
-        <img id="down" src="./images/main_02_down_off.png" alt="">
-        <img id="copy" src="./images/main_06_more_off.png" alt="">
-        <img id="delete" src="./images/main_03_garbage_off.png" alt="">
-    </div>
-    <div class="radioSubject">
-        <div class="radioStemText textBox" id="radioStemTextID">单选题</div>
-        <ul class="radioItem">
-            <li>
-                <input type="radio" name="radio1" id="Num1" />
-                <label class="textBox radioItemText">选项1</label>
-            </li>
-            <li>
-                <input type="radio" name="radio1" id="Num2" />
-                <label class="textBox radioItemText">选项2</label>
-            </li>
-        </ul>
-        <img id="addItem" src="./images/main_04_add_off.png" alt="">
-    </div>
-</div>`;
