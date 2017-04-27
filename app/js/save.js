5548d2540ced8f2db233e70de4fa2627d3e9a4e9
Emily <xuheyao_emily@163.com>
2017-04-27 15:26:42 +0800

    预记录一下之前的修改

--- app/js/save.js
+++ app/js/save.js
@@ -1,5 +1,6 @@
 var mainData = null;
-var tempGUID = null;
+// var tempParentGUID = null;
+// var tempParent = null;
 
 function saveQuestionnaire() {
     console.log("正在保存");
@@ -53,6 +54,8 @@ function getQuestionnaireJson(cb) {
 
 function getPatternJson(cb) {
     var flag = 1;
+    var map = {};
+    var Subjects = null;
 
     var questionnaireData = {
         "guid": tempQuestionnaire.recid,
@@ -64,48 +67,81 @@ function getPatternJson(cb) {
         "mainBodyGuid": "",
         "css": ""
     }
-    var Subjects = $("#target .subject, #target .unSubject");
+    Subjects = $("#target .subject, #target .unSubject");
+    // var map = {};
+    //
+    // for (var i = 0; i < Subjects.length; i++) {
+    //     var type = getType(Subjects.eq(i));
+    //     var tempSubject = getSubjectJson(Subjects.eq(i), type);
+    //     map[Subjects.eq(i).attr("guid")] = Subjects[i];
+    // }
+    //
+    // for(var i = 0; i < Subjects.length; i++){
+    //     var guid = Subjects.eq(i).parent().parent().parent(".mergeDiv").eq(0).attr("guid");
+    //     if(guid !== undefined){
+    //         if(!map[guid].questions){
+    //             map[guid].questions = [];
+    //         }
+    //         console.log(guid);console.log(map[guid]);
+    //         map[guid].questions.push(map[Subjects.eq(i).attr("guid")]);
+    //     }else{
+    //         questionnaireData.questions.push(Subjects[i]);
+    //     }
+    // }
+    //
+    // console.log("***********************************************************");
+    // console.log(questionnaireData);
+
+    // for (var i = 0; i < Subjects.length; i++) {
+    //     console.log(Subjects.eq(i));
+    //
+    //     var type = getType(Subjects.eq(i));
+    //     var tempSubject = getSubjectJson(Subjects.eq(i), type);
+    //
+    //     if (typeof(Subjects.eq(i).parent().parent().parent(".mergeDiv").eq(0).attr("class")) == "string") {
+    //         /*父节点为合并题 深度遍历*/
+    //         var tempParentGUID = Subjects.eq(i).parent().parent().parent(".mergeDiv").eq(0).attr("guid");
+    //
+    //         traverse(tempParentGUID, questionnaireData, 0, function(res) {
+    //             if (res.success == true) {
+    //                 res.data.questions.push(tempSubject);
+    //                 if (i == Subjects.length - 1) {
+    //                     console.log(JSON.stringify(questionnaireData));
+    //                 }
+    //             }
+    //         })
+    //     } else {
+    //         questionnaireData.questions.push(tempSubject);
+    //         if (i == Subjects.length - 1) {
+    //             console.log(JSON.stringify(questionnaireData));
+    //         }
+    //     }
+    //
+    // }
+    console.log("============================================");
+
+
     for (var i = 0; i < Subjects.length; i++) {
         var type = getType(Subjects.eq(i));
         var tempSubject = getSubjectJson(Subjects.eq(i), type);
-        console.log(type);
-        console.log(tempSubject);
-        // console.log(Subjects.eq(i).parent().parent().parent(".mergeDiv").eq(0).attr("class"));
 
-        if (typeof(Subjects.eq(i).parent().parent().parent(".mergeDiv").eq(0).attr("class")) == "string") {
-            /*父节点为合并题 深度遍历*/
-            tempLength = questionnaireData.questions.length;
-            tempJson = questionnaireData.questions[tempLength - 1];
-            var flag = 1;
-
-            while (tempJson.type == "group" && flag == 1) {
-                console.log("这是一道合并题");
-                tempLength2 = tempJson.questions.length;
-                if (tempLength2 > 0) {
-                    tempJson2 = tempJson.questions[tempLength2 - 1];
-                    if (tempJson2.type != "group") {
-                        console.log("添加到该层");
-                        tempJson.questions.push(tempSubject);
-                        flag = 0;
-                    } else {
-                        /*继续向下深度遍历*/
-                        console.log("继续向下深度遍历");
-                        tempLength = tempLength2;
-                        tempJson = tempJson2;
-                    }
-                } else {
-                    /*该合并题中没有题目 直接添加到该合并题下*/
-                    console.log("该合并题中还没有题 直接添加");
-                    tempJson.questions.push(tempSubject);
-                    flag = 0;
-                }
-            }
-        } else {
-            questionnaireData.questions.push(tempSubject);
-        }
+        tempSubject["i"] = i;
+        map[Subjects.eq(i).attr("guid")] = tempSubject;
     }
 
+    for (var i = 0; i < Subjects.length; i++) {
+        var tempParent = Subjects.eq(i).parent().parent().parent(".mergeDiv").eq(0);
+        var tempGUID = Subjects.eq(i).attr("guid");
+        if (typeof(tempParent.attr("class")) == "string") {
+            console.log("该题的爹为合并题");
+            map[tempParent.attr("guid")].questions.push(map[tempGUID]);
+        } else {
+            console.log("该题为最外层题目");
+            questionnaireData.questions.push(map[tempGUID]);
+        }
+    }
     console.log(JSON.stringify(questionnaireData));
+
     cb({
         success: true,
         data: questionnaireData
@@ -118,18 +154,17 @@ function getSubjectJson($td, type) {
     if (tempGUID == "" || tempGUID == null || tempGUID == undefined) {
         tempGUID = newGuid();
         $td.attr("guid", tempGUID);
-        console.log("hehe");
     }
 
     if (type == "merge") {
         tempSubject = {
             "title": $td.children().children(".stemText").html(),
+            "type": "group",
             "level": "level" + $td.attr("level"),
             "hidden": false,
             "description": "",
             "questions": [],
             "question": tempGUID,
-            "type": "group",
             "levelNum": $td.children().children("h4").html()
         };
     } else if (type == "radio") {
@@ -144,7 +179,7 @@ function getSubjectJson($td, type) {
             "optionlayout": 1,
             "question": tempGUID,
             "type": "single",
-            "levelNum": "",
+            "levelNum": $td.children().children("h4").html(),
             "options": [],
         };
         var options = $td.children().children(".itemBox").children("li");
@@ -267,4 +302,27 @@ function getSubjectJson($td, type) {
         };
     }
     return tempSubject;
+}
+
+function traverse(tempParentGUID, node, i, cb) {
+    var children = node.questions;
+    if (children != null && children[i] != null) {
+
+        if (children[i].question == tempParentGUID) {
+            // tempParent = children[i];
+            console.log("parent:" + node.levelNum + ", child:" + children[i].levelNum);
+            console.log("parent:" + node.question + ", child:" + children[i].question);
+            cb({
+                success: true,
+                data: children[i]
+            });
+        }
+        if (i == children.length - 1) {
+            console.log("进入孩子节点 " + node.levelNum + children[i].levelNum);
+            traverse(tempParentGUID, children[0], 0, cb);
+        } else {
+            console.log("进入兄弟节点 " + node.levelNum);
+            traverse(tempParentGUID, node, i + 1, cb);
+        }
+    }
 }
